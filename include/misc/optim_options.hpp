a39b5a8cc6e80f1b230698c4fc5e31bed61a623a
Enrico <enrico.detoma@gmail.com>
2022-06-04 16:59:41 +0200

    Include OpenMP

--- include/misc/optim_options.hpp
+++ include/misc/optim_options.hpp
@@ -43,18 +43,6 @@
 
 //
 
-#if defined(_OPENMP) && !defined(OPTIM_DONT_USE_OPENMP)
-    #undef OPTIM_USE_OPENMP
-    #define OPTIM_USE_OPENMP
-#endif
-
-#if !defined(_OPENMP) && defined(OPTIM_USE_OPENMP)
-    #undef OPTIM_USE_OPENMP
-
-    #undef OPTIM_DONE_USE_OPENMP
-    #define OPTIM_DONE_USE_OPENMP
-#endif
-
 #ifdef OPTIM_USE_OPENMP
     #include "omp.h" //  OpenMP
 #endif
